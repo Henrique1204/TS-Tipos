04b1333d6b9e0b766a40b4351c54ccc6a5c07d90
Paulo <pauloh16.pdr@gmail.com>
2021-05-01 20:28:30 -0300

    Adicionar conteudo da aula 'Chaves em tipo'

---
+++ src/Chaves-em-tipo/ndex.ts
@@ -0,0 +1,18 @@
+type Veiculo = {
+  marca: string;
+  ano: number;
+};
+
+type Car = {
+  brand: Veiculo['marca'];
+  year: Veiculo['ano'];
+  name: string;
+};
+
+const carro: Car = {
+  brand: 'Ford',
+  year: 2021,
+  name: 'Celta',
+};
+
+console.log(carro);
